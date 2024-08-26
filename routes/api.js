12d8bac8b4753bdddd6865585e1d78a872df8490
Derek Mudie <delm83@googlemail.com>
2024-08-26 12:30:51 +0000

    added post and get methods

--- routes/api.js
+++ routes/api.js
@@ -46,11 +46,41 @@ module.exports = function (app) {
    try{
     let board = req.params.board;
     let Thread = mongoose.model(board, threadSchema);
-    let issue_list = await Thread.find().select({__v: 0, reported: 0, delete_password: 0});
-    return res.json(issue_list);
+    let thread_list = await Thread.find().select({__v: 0, reported: 0, delete_password: 0}).sort({ bumped_on: -1 }).limit(10);
+    console.log(thread_list);
+    for(let x=0; x<thread_list.length; x++){
+      thread_list[x].replies = thread_list[x].replies.slice(-3);
+      console.log(thread_list[x]);
+    }
+    return res.json(thread_list);
  }catch(err){return res.json({error: err})}
  });
     
-  app.route('/api/replies/:board');
-
+  app.route('/api/replies/:board').post(async (req, res)=>{
+    try{
+      let board = req.params.board;
+      let Thread = mongoose.model(board, threadSchema);
+      let thread_id = req.body.thread_id;
+      let text = req.body.text
+      let delete_password = req.body.delete_password
+      let input_thread = await Thread.findById(thread_id);
+      let inputReply = new Thread({
+       text: text,
+       delete_password: delete_password,
+       created_on: new Date(),
+ });
+   input_thread.bumped_on = new Date();
+   input_thread.replies.push({"_id": inputReply._id, "text": inputReply.text, "created_on": inputReply.created_on});
+   await input_thread.save();
+   return res.redirect('/b/'+board+'/'+thread_id);
+  }catch(err){return res.json({error: err})}
+  }).get(async (req, res)=>{
+    try{
+     let board = req.params.board;
+     let Thread = mongoose.model(board, threadSchema);
+     let thread_id = req.query.thread_id;
+     let input_thread = await Thread.findById(thread_id).select({__v: 0, reported: 0, delete_password: 0});
+     return res.json(input_thread);
+  }catch(err){return res.json({error: err})}
+  });
 };
